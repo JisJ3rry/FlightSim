8397a7d5e8ed401aca22677e82d82786a3dce49b
MrIndeciso <mrindeciso@libero.it>
2018-05-01 10:36:56 +0200

    Hopefully now the random function works. The Settings menu is almost ready. The secret function that removes 3 is still WIP. Committing so that if Win 10 breaks something I don't lose any progress.
    Signed-off-by: MrIndeciso <mrindeciso@libero.it>

--- app/src/main/java/com/school/mrind/flightsim/FlightSim.java
+++ app/src/main/java/com/school/mrind/flightsim/FlightSim.java
@@ -1,9 +1,14 @@
 //TODO Convertire tutto ad operazioni asincrone
 //TODO Capire che non si può convertire tutto ad operazioni asincrone
+//TODO Aggiungere commenti a tutto
+//TODO Aggiungere link di dove ho copiato il codice
+//TODO Rassegnarsi e non commentare niente
 
 package com.school.mrind.flightsim;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,8 +20,10 @@ import com.school.mrind.flightsim.DBFiles.DBInitializer;
 import com.school.mrind.flightsim.DBFiles.User;
 import com.school.mrind.flightsim.databinding.ActivityMainBinding;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class FlightSim extends AppCompatActivity {
 
@@ -34,7 +41,8 @@ public class FlightSim extends AppCompatActivity {
         selectclass(activityMainBinding);
 
         activityMainBinding.button2.setOnClickListener((View v) -> {
-            nuke();
+            //nuke();
+            generate(activityMainBinding);
         });
 
         activityMainBinding.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -42,7 +50,7 @@ public class FlightSim extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 List<User> usr = DB.getAppDatabase(FlightSim.this).userDao().loadAllByClassnum(parent.getSelectedItem().toString());
                 for(User us : usr){
-                    activityMainBinding.editText.append(us.nome);
+                    activityMainBinding.editText.append(us.getStudentnum() + "   " + us.nome + (us.isInterrogato() ? "    Sì" : "    No"));
                 }
             }
 
@@ -72,7 +80,34 @@ public class FlightSim extends AppCompatActivity {
         activityMainBinding.spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, classtype));
     }
 
+    private void generate(ActivityMainBinding activityMainBinding){
+        ArrayList<String> nomi = new ArrayList<>();
+        ArrayList<Integer> numeri = new ArrayList<>();
+        List<User> usr = DB.getAppDatabase(this).userDao().loadAllByClassnum(activityMainBinding.spinner.getSelectedItem().toString());
+        for(User user : usr){
+            if(!user.isInterrogato()){
+                nomi.add(user.getNome());
+                numeri.add(user.getStudentnum());
+            }
+        }
+        byte[] bt = new byte[6];
+        bt = activityMainBinding.editText2.getText().toString().getBytes(Charset.defaultCharset());
+        long result = 0;
+        for (int i = 0; i < 8; i++) {
+            result <<= 8;
+            result |= (bt[i] & 0xFF);
+        }
+        Random rnd = new Random(result);
+        int blyat = rnd.nextInt(nomi.size());
+        activityMainBinding.editText.setText("");
+        activityMainBinding.editText.append(nomi.get(blyat) + numeri.get(blyat));
+    }
+
     public void nuke(){
         DB.getAppDatabase(this).userDao().deleteAll();
+    }
+
+    private void openGitHub(){
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/MrIndeciso/FlightSim")));
     }
 }
